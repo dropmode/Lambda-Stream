48f0216be518e218f4e7d0ac4d20ea1151e980ad
MItsuki Shishido <169013117+dropmode@users.noreply.github.com>
2024-06-07 00:40:01 +0900

    課題提出

--- src/practice/Chapter10.java
+++ src/practice/Chapter10.java
@@ -3,28 +3,36 @@ package practice;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Chapter10 {
-    public static void main(String[] args) throws Exception {
-        List<Task> list = new ArrayList<>();
-        list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
-        list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
-        list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
-        list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
-        list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+	public static void main(String[] args) throws Exception {
+		List<Task> list = new ArrayList<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
 
-        // 以下記述
-        // 1. 未完了のタスクの個数を表示
-        // 2. 未完了のタスク一覧を日時の昇順に並べて表示
-        
-        list.stream()
-        
-        .filter(l -> l.isDone() == false).count()
-        
-        
-        
-        
-        
-        
-    }
+		// 以下記述
+		// 1. 未完了のタスクの個数を表示
+		// 2. 未完了のタスク一覧を日時の昇順に並べて表示
+
+		long n =list.stream()
+		.filter(l -> l.isDone() == false)
+		.count();
+		
+		Consumer<String> t1 = System.out::println;
+		t1.accept("未完了のタスクの個数は" + n);
+		
+		System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+		
+		list.stream()
+		.filter(l -> l.isDone() == false)
+		.sorted()
+		.forEach(System.out::println);
+		
+		
+
+	}
 }
